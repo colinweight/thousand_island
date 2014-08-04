9f77bc783c4f958f2d7923ee94ba20f6f5cf3328
Colin Weight <colin@alphamac.com.au>
2014-08-04 11:49:11 +1000

    Modifications to Template
    - doc, document naming changed to pdf. ie. pdf_options
    
    - Added draw_xxxx methods for header, body, and footer. Each calls the
    method on the relevant component (except body), and passes the templates
    own header/footer method in a block if it exists.

--- lib/thousand_island/template.rb
+++ lib/thousand_island/template.rb
@@ -1,34 +1,57 @@
 module ThousandIsland
   class Template
 
-    attr_reader :doc, :doc_options
+    attr_reader :pdf, :pdf_options
 
     def initialize(options={})
       setup_document_options(options)
       setup_prawn_document
     end
 
-    def setup_prawn_document
-      @doc = Prawn::Document.new(doc_options)
-    end
-
-    def setup_document_options(options={})
-      @doc_options = document_defaults.merge(options)
-    end
-
-    def document_defaults
+    def defaults
       {
         page_size: 'A4',
         page_layout: :portrait,
         left_margin: 54,
         right_margin: 54,
         top_margin: 56,
-        bottom_margin: 80
+        bottom_margin: 80,
+        header: {render: true},
+        footer: {render: true},
       }
     end
 
-    def header
-      @header ||= Header.new(doc, doc_options)
+    def draw_body(&block)
+      body &block if respond_to? :body
+    end
+
+    def draw_header(&block)
+      header_obj.draw do
+        header &block if respond_to? :header
+      end if pdf_options[:header][:render]
+    end
+
+    def draw_footer(&block)
+      footer_obj.draw do
+        footer &block if respond_to? :footer
+      end if pdf_options[:footer][:render]
+    end
+
+    def setup_document_options(options={})
+      @pdf_options = defaults.merge(options)
+    end
+
+  private
+    def setup_prawn_document
+      @pdf = Prawn::Document.new(pdf_options)
+    end
+
+    def header_obj
+      @header ||= Components::Header.new(pdf, pdf_options[:header])
+    end
+
+    def footer_obj
+      @footer ||= Components::Footer.new(pdf, pdf_options[:footer])
     end
 
   end
--- spec/thousand_island/template_spec.rb
+++ spec/thousand_island/template_spec.rb
@@ -1,5 +1,6 @@
 module ThousandIsland
   describe Template do
+
     context 'setup' do
       it { expect(subject.pdf).to be_a(Prawn::Document) }
 
@@ -9,6 +10,72 @@ module ThousandIsland
         subject.setup_document_options(options)
         expected = { op_one: 1, op_two: 2, op_three: 3 }
         expect(subject.pdf_options).to eq(expected)
+      end
+    end
+
+    context 'Header' do
+      let(:header) { instance_double('ThousandIsland::Components::Header') }
+
+      before :each do
+        allow(header).to receive(:draw)
+        allow(subject).to receive(:header_obj) { header }
+      end
+
+      it 'calls draw on the Header component' do
+        expect(header).to receive(:draw)
+        subject.draw_header
+      end
+
+      it 'does not call self.header if not exists' do
+        expect(subject).to_not receive(:header)
+        subject.draw_header
+      end
+
+      it 'calls self header when it exists' do
+        described_class.send(:define_method, :header) {}
+        template = described_class.new
+        expect(template).to receive(:header)
+        template.draw_header
+      end
+    end
+
+    context 'Footer' do
+      let(:footer) { instance_double('ThousandIsland::Components::Footer') }
+
+      before :each do
+        allow(footer).to receive(:draw)
+        allow(subject).to receive(:footer_obj) { footer }
+      end
+
+      it 'calls draw on the Footer component' do
+        expect(footer).to receive(:draw)
+        subject.draw_footer
+      end
+
+      it 'does not call self.footer if not exists' do
+        expect(subject).to_not receive(:footer)
+        subject.draw_footer
+      end
+
+      it 'calls self footer when it exists' do
+        described_class.send(:define_method, :footer) {}
+        template = described_class.new
+        expect(template).to receive(:footer)
+        template.draw_footer
+      end
+    end
+
+    context 'Body' do
+      it 'does not call self.body if not exists' do
+        expect(subject).to_not receive(:body)
+        subject.draw_footer
+      end
+
+      it 'calls self body when it exists' do
+        described_class.send(:define_method, :body) {}
+        template = described_class.new
+        expect(template).to receive(:body)
+        template.draw_body
       end
     end
 
