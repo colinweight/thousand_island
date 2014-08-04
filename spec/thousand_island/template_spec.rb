ee2ff2ec67b98eafdbc5e5b846a629a5c2ca85c3
Colin Weight <colin@alphamac.com.au>
2014-08-04 10:41:45 +1000

    Renamed document_spec to template_spec

--- spec/thousand_island/template_spec.rb
+++ spec/thousand_island/template_spec.rb
@@ -1,14 +1,14 @@
 module ThousandIsland
   describe Template do
     context 'setup' do
-      it { expect(subject.doc).to be_a(Prawn::Document) }
+      it { expect(subject.pdf).to be_a(Prawn::Document) }
 
       it 'sets the options with merged defaults and passed options' do
         options = { op_one: 1, op_two: 2 }
-        allow(subject).to receive(:document_defaults) { { op_two: 22, op_three: 3 } }
+        allow(subject).to receive(:defaults) { { op_two: 22, op_three: 3 } }
         subject.setup_document_options(options)
         expected = { op_one: 1, op_two: 2, op_three: 3 }
-        expect(subject.doc_options).to eq(expected)
+        expect(subject.pdf_options).to eq(expected)
       end
     end
 
