52f1e52bb39655a5ca2c839a30a397c717901d19
Colin Weight <colin@alphamac.com.au>
2014-08-08 17:09:34 +1000

    StyleSheet is now a Module, not a Class
    - The way it was being used by the Template lends itself much better as
    a module that is included in the Template.
    
    - Rather than meta programming the x_style methods from the defaults,
    the defaults are no longer needed, so styles are defined directly. This
    makes it easy to merge them to new styles in Templates.
    
    - All styles are now StyleHash objects instead of vanilla Hash. This
    adds extra keys for Prawn.formatted_text without forcing the user to
    remember to define them (:font_size returns the :size value)

--- lib/thousand_island/style_sheet.rb
+++ lib/thousand_island/style_sheet.rb
@@ -1,106 +1,79 @@
 module ThousandIsland
-  class StyleSheet
+  module StyleSheet
 
-    class << self
-      attr_writer :available_styles
-
-      def available_styles
-        @available_styles ||= []
-      end
-    end
-
-    def available_styles
-      self.class.available_styles
-    end
-
-    def defaults
-      {
+    def default_style
+      StyleHash.new({
           size: 10,
           style: :normal,
           align: :left,
           leading: 1,
           inline_format: true,
           color: '000000',
-      }
+      })
     end
 
-    def body_defaults
-      defaults
+    def body_style
+      default_style
     end
 
-    def h1_defaults
-      defaults.merge({
-          size: defaults[:size] * 2.5,
+    def h1_style
+      default_style.merge({
+          size: default_style[:size] * 2.5,
           style: :bold,
           leading: 8,
       })
     end
 
-    def h2_defaults
-      defaults.merge({
-          size: defaults[:size] * 2.14,
+    def h2_style
+      default_style.merge({
+          size: default_style[:size] * 2.14,
           style: :bold,
           leading: 2,
       })
     end
 
-    def h3_defaults
-      defaults.merge({
-          size: defaults[:size] * 1.7,
+    def h3_style
+      default_style.merge({
+          size: default_style[:size] * 1.7,
           style: :bold,
       })
     end
 
-    def h4_defaults
-      defaults.merge({
-          size: defaults[:size] * 1.3,
+    def h4_style
+      default_style.merge({
+          size: default_style[:size] * 1.3,
           style: :bold_italic,
       })
     end
 
-    def h5_defaults
-      defaults.merge({
-          size: defaults[:size] * 1,
+    def h5_style
+      default_style.merge({
+          size: default_style[:size] * 1,
       })
     end
 
-    def h6_defaults
-      defaults.merge({
-          size: defaults[:size] * 0.85,
+    def h6_style
+      default_style.merge({
+          size: default_style[:size] * 0.85,
           style: :italic,
       })
     end
 
-    def footer_defaults
-      defaults.merge({
-         size: defaults[:size] * 0.8,
+    def footer_style
+      default_style.merge({
+         size: default_style[:size] * 0.8,
          color: '666666',
          align: :center,
      })
     end
 
-    def default_style
-      defaults.merge({
-        font_size: defaults[:size],
-        styles: [defaults[:style]],
-      })
+    available_styles = []
+    instance_methods.grep(/_style$/).each do |method_name|
+      style = method_name.to_s.sub('_style', '')
+      available_styles << style.to_sym unless style == 'default'
     end
-
-
-    #Create a method for each of the known styles from the defaults
-    instance_methods.grep(/_defaults$/).each do |method_name|
-      method_name = method_name.to_s
-      style = method_name.sub('_defaults', '')
-      available_styles << style.to_sym
-      style_method_name = "#{style}_style"
-      define_method(style_method_name) do
-        defaults = method(method_name).()
-        new_keys = {
-            font_size: defaults[:size],
-            styles: [defaults[:style]],
-        }
-        defaults.merge(new_keys)
-      end
+    define_method(:available_styles) do
+      available_styles
     end
 
   end
--- spec/thousand_island/style_sheet_spec.rb
+++ spec/thousand_island/style_sheet_spec.rb
@@ -1,18 +1,26 @@
 module ThousandIsland
   describe StyleSheet do
-
+    let(:subject) do
+      dummy = Class.new
+      dummy.include(described_class)
+      dummy.new
+    end
     let(:expected_styles) {  [:h1, :h2, :h3, :h4, :h5, :h6, :body, :footer] }
 
     it 'builds the available styles array' do
       expect(subject.available_styles).to match_array(expected_styles)
     end
 
+    it 'has a style method for every style' do
+      expected_styles.each do |s|
+        expect(subject.send("#{s}_style")).to be_a(StyleHash)
+      end
+    end
+
     it 'merges body_style with custom for h1' do
       expected = {
           size: 10 * 2.5,
-          font_size: 10 * 2.5,
           style: :bold,
-          styles: [:bold],
           align: :left,
           leading: 8,
           inline_format: true,
@@ -21,16 +29,15 @@ module ThousandIsland
       expect(subject.h1_style).to eq(expected)
     end
 
-    it 'responds to h1_style' do
-      expect(subject.respond_to?(:h1_style)).to be true
-    end
-
-    it 'responds to h2_style' do
-      expect(subject.respond_to?(:h2_style)).to be true
-    end
-
-    it 'responds to h3_style' do
-      expect(subject.respond_to?(:h3_style)).to be true
+    context 'extends the Hash with' do
+      it 'font_size' do
+        style = subject.h1_style
+        expect(style[:font_size]).to eq(style[:size])
+      end
+      it 'styles' do
+        style = subject.h3_style
+        expect(style[:styles]).to eq(style[:style])
+      end
     end
 
   end
